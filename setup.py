ae9d36f0351dd363834b45cbd9770c17f0818a76
John Mille <john@ews-network.net>
2019-06-17 10:18:37 +0100

    Fixed: Version names to allow use of bumpversion

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/johnpreston/ozone',
-    version=__version__,
+    version='0.1.0',
     zip_safe=False,
 )
